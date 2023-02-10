ae7b332972755c73c4637dc6c25aabce5a7d46b7
bm.sanjeet2021@gmail.com <72962798+SanjeetBalamurugan@users.noreply.github.com>
2023-02-10 08:16:18 +0530

    added navbar

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+include "./components/navbar.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home - Spice Valley School</title>
+  <link rel="stylesheet" href="assets/globals.css">
+  <link rel="stylesheet" href="assets/index.css">
+</head>
+
+<body>
+  <div class="wrapper-img">
+    <span>Learn from yesterday, live for today, hope for tomorrow.</span>
+  </div>
+  <script src="assets/index.js"></script>
+</body>
+
+</html>
